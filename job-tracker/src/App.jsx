86db8ca2e87a797abbb3f884c01b0984c57f0bda
Hugo Rodrigues <hugo.rdg.dev@gmail.com>
2025-09-20 15:57:52 +0200

    Add minimal App shell

---
+++ job-tracker/src/App.jsx
@@ -0,0 +1,6 @@
+<div className="p-4 bg-blue-500 text-white">Tailwind OK ?</div>
+import { JobApplicationTracker } from './features/jobApplications';
+
+const App = () => <JobApplicationTracker />;
+
+export default App;
