b7d617a13bb4b1d4591409bec58d5528c684058f
Hugo Rodrigues <hugo.rdg.dev@gmail.com>
2025-10-03 07:58:35 +0200

    Removed the "priority" part throughout the options

--- job-tracker/src/features/jobApplications/constants/options.js
+++ job-tracker/src/features/jobApplications/constants/options.js
@@ -1,3 +1,2 @@
 export const STATUS_OPTIONS = ['A Envoyer', 'En cours', 'Entretien', 'Acceptée', 'Refusée'];
 export const TYPE_OPTIONS = ['Offre', 'Spontanée'];
-export const PRIORITY_OPTIONS = ['Haute', 'Moyenne', 'Basse'];
