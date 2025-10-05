626033b16e7e1f864d89cc7c16ddc09246651355
Hugo Rodrigues <hugo.rdg.dev@gmail.com>
2025-10-05 09:56:18 +0200

    Set up default Companies Categories

---
+++ job-tracker/src/features/jobApplications/constants/companyCategories.js
@@ -0,0 +1,5 @@
+export const DEFAULT_COMPANY_CATEGORIES = [
+  { id: 'priority', labelKey: 'companies.defaults.priority' },
+  { id: 'explore', labelKey: 'companies.defaults.explore' },
+  { id: 'watchlist', labelKey: 'companies.defaults.watchlist' },
+];
